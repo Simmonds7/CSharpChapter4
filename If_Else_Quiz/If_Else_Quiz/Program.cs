fecd9126dd5d60f3dc54c8647df3b2abf0bd4949
Simmonds7 <44840687+Simmonds7@users.noreply.github.com>
2018-11-19 12:11:40 -0600

    upload IfElse

---
+++ If_Else_Quiz/If_Else_Quiz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace If_Else_Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pick;
+
+            WriteLine("Welcome to the Prize Winning Door Program, P.W.D.P. Please pick a door, 1-3.");
+            pick = Convert.ToInt32(ReadLine());
+
+            if (pick == 2)
+
+            {
+                WriteLine("Congratulations! You have successfully picked the correct door");
+            }
+            else if (pick == 1 || pick == 3)
+            {
+                WriteLine("Congratulations! On not being correct!");
+            }
+            else if (pick != 1 && pick != 2 && pick != 3)
+            {
+                WriteLine("Error (Number not recognized). Please Restart the program, this time taking care to follow directions.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
