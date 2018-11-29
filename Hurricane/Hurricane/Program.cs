ea37b815482e4503c5a82bbac1fbb24ef51ec874
Simmonds7 <44840687+Simmonds7@users.noreply.github.com>
2018-11-29 12:43:38 -0600

    hurricane

---
+++ Hurricane/Hurricane/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Hurricane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int windSpeed;
+            
+
+            WriteLine("Welcome to the Hurricane Category Placeing Program, H.C.P.P. Input the windspeed of the current storm.");
+            windSpeed = Convert.ToInt32(ReadLine());
+
+            if (windSpeed < 74)
+            {
+                WriteLine("Current storm is not a hurricane.");
+            }
+            else if (windSpeed >= 74 && windSpeed < 96)
+            {
+                WriteLine("Current storm is a category 1 hurricane.");
+            }
+            else if (windSpeed >= 96 && windSpeed < 111)
+            {
+                WriteLine("Current storm is a category 2 hurricane.");
+            }
+            else if (windSpeed >= 111 && windSpeed < 130)
+            {
+                WriteLine("Current storm is a category 3 hurricane.");
+            }
+            else if (windSpeed >= 130 && windSpeed < 157)
+            {
+                WriteLine("Current storm is a category 4 hurricane.");
+            }
+            else if (windSpeed >= 157)
+            {
+                WriteLine("WARNING! CURRENT STORM IS A CATEGORY 5 HURRICANE!");
+            }
+            
+        }
+    }
+}
